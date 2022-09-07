0a719c5582d8fe963b7aa3209c7f84ae6066de3a
Bercy Burgos <103054429+bercyburgos@users.noreply.github.com>
2022-09-06 21:14:32 -0500

    Update app.js
    Agrege condicional OR y AND

--- app.js
+++ app.js
@@ -15,5 +15,25 @@ function getContactInfo({nombre,Apellido,correo}){
 
 document.write("El nombre completo del usuario es "+getContactInfo(datosUsuario))
 
+//Creacion de una copia del objeto
+
+let copyOfuser={...datosUsuario};
+
+console.log({copyOfuser});
+
+//Operador Logico AND
+
+let Primercontacto=true;
+let Segundocontacto=false;
+let resultadoFinal= Primercontacto&&Segundocontacto;
+
+console.log("El resultado final de la validacion del contacto es: "+resultadoFinal)
+
+//Operador Logico OR
+
+let resultadoOR =Primercontacto||Segundocontacto;
+
+console.log("El resultado final de la validacion del contacto usando OR es: "+resultadoOR)
+
 
 
